ac7f7e2a69cc42a6e1b5d4c146e1014748687801
Junaid Bashir <junaid@crayondevelopers.com>
2020-11-12 21:32:37 +0100

    add lessons.js add lesson details link html to lessons.js to view all lessons

---
+++ lessons.js
@@ -0,0 +1,110 @@
+let lessons = [
+    {
+        "id": 01,
+        "subject": 'Accounting',
+        "location": "Mauritius",
+        "price": 2000,
+        "image": "img/accounting.jpg",
+        "availableInventory": 5,
+        "rating": 3,
+    },
+    {
+        "id": 02,
+        "subject": 'Chemistry',
+        "location": "Dubai",
+        "price": 3500,
+        "image": "img/chemistry.jpg",
+        "availableInventory":5,
+        "rating": 4
+    },
+    {
+        "id": 03,
+        "subject": 'Biology',
+        "location": "Malta",
+        "price": 10000,
+        "image": "img/biology.jpg",
+        "availableInventory":5,
+        "rating": 5
+    },
+    {
+        "id": 04,
+        "subject": 'Government',
+        "location": "Mauritius",
+        "price": 2000,
+        "image": "img/government.jpg",
+        "availableInventory": 5,
+        "rating": 3,
+    },
+    {
+        "id": 05,
+        "subject": 'English',
+        "location": "Dubai",
+        "price": 3500,
+        "image": "img/english.jpg",
+        "availableInventory":5,
+        "rating": 4
+    },
+    {
+        "id": 06,
+        "subject": 'Law',
+        "location": "Malta",
+        "price": 10000,
+        "image": "img/law.jpg",
+        "availableInventory":5,
+        "rating": 5
+    },
+    {
+        "id": 07,
+        "subject": 'History',
+        "location": "Mauritius",
+        "price": 2000,
+        "image": "img/history.jpg",
+        "availableInventory": 5,
+        "rating": 3,
+    },
+    {
+        "id": 08,
+        "subject": 'Science',
+        "location": "Dubai",
+        "price": 3500,
+        "image": "img/science.jpg",
+        "availableInventory":5,
+        "rating": 4
+    },
+    {
+        "id": 09,
+        "subject": 'Math',
+        "location": "Malta",
+        "price": 10000,
+        "image": "img/math.jpg",
+        "availableInventory":5,
+        "rating": 5
+    },
+    {
+        "id": 010,
+        "subject": 'Mechanics',
+        "location": "Mauritius",
+        "price": 2000,
+        "image": "img/demo-img.svg",
+        "availableInventory": 5,
+        "rating": 3,
+    },
+    {
+        "id": 011,
+        "subject": 'Auto repair',
+        "location": "Dubai",
+        "price": 3500,
+        "image": "img/demo-img.svg",
+        "availableInventory":5,
+        "rating": 4
+    },
+    {
+        "id": 012,
+        "subject": 'Civil Engineering',
+        "location": "Hendon",
+        "price": 10000,
+        "image": "img/demo-img.svg",
+        "availableInventory":5,
+        "rating": 5
+    }
+]
